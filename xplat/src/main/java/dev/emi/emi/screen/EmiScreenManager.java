353de2afb5e13d8a4775b90c4a2be4e9df250237
a.o.glazkov <a.o.glazkov@aspireteam.ru>
2025-12-26 21:01:13 +0300

    Add multiple recipe tree management and batch manipulation
    Enhance recipe tree functionality with support for multiple trees, allowing addition, removal, and selection. Add combinable costs and progression for aggregated handling. Introduce new tooltips for advanced batch adjustments with `Ctrl` and `Alt`.
    
    # Conflicts:
    #	xplat/src/main/java/dev/emi/emi/screen/BoMScreen.java

--- xplat/src/main/java/dev/emi/emi/screen/EmiScreenManager.java
+++ xplat/src/main/java/dev/emi/emi/screen/EmiScreenManager.java
@@ -858,7 +858,7 @@ public class EmiScreenManager {
 		}
 		Set<Slot> ignoredSlots = Sets.newHashSet();
 		Set<EmiStack> synfavs = Sets.newHashSet();
-		if (BoM.craftingMode && BoM.tree != null) {
+		if (BoM.craftingMode && BoM.getTree() != null) {
 			List<EmiFavorite.Synthetic> syntheticFavorites = EmiFavorites.syntheticFavorites;
 			for (EmiFavorite.Synthetic fav : syntheticFavorites) {
 				synfavs.addAll(fav.getEmiStacks());
@@ -887,13 +887,13 @@ public class EmiScreenManager {
 				context.push();
 				context.matrices().translate(0, 0, 300);
 				if (query != null) {
-					if (!query.test(stack)) {
-						context.fill(slot.x - 1, slot.y - 1, 18, 18, 0x77000000);
-					}
-				} else if (BoM.craftingMode && BoM.tree != null) {
-					if (!(slot.inventory instanceof PlayerInventory) && !ignoredSlots.contains(slot) && synfavs.contains(stack)) {
-						context.fill(slot.x - 1, slot.y - 1, 18, 18, 0x7700BBFF);
-					}
+ 				if (!query.test(stack)) {
+ 					context.fill(slot.x - 1, slot.y - 1, 18, 18, 0x77000000);
+ 				}
+ 			} else if (BoM.craftingMode && BoM.getTree() != null) {
+ 				if (!(slot.inventory instanceof PlayerInventory) && !ignoredSlots.contains(slot) && synfavs.contains(stack)) {
+ 					context.fill(slot.x - 1, slot.y - 1, 18, 18, 0x7700BBFF);
+ 				}
 				}
 				context.pop();
 			}
